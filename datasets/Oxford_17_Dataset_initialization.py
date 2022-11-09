0fba7acc26bac455c0174afd2aaeb316d3f003e0
Wu Kunhuan <u3577163@connect.hku.hk>
2022-11-09 18:17:04 +0800

    [Update] Stage 1

--- datasets/Oxford_17_Dataset_initialization.py
+++ datasets/Oxford_17_Dataset_initialization.py
@@ -2,113 +2,139 @@
 import numpy as np
 import os, shutil
 
-def retrieve_info(file, info_header, error_message): 
-    try: 
-        file_read = open(file, "r")
-    except FileNotFoundError: 
-        raise Exception(f"[Error] {file} does not exist. {error_message}")
+class Oxford_17_Dataset: 
+    
+            
+    def retrieve_info(self, file, info_header, error_message): 
+        try: 
+            file_read = open(file, "r")
+        except FileNotFoundError: 
+            raise Exception(f"[Error] {file} does not exist. {error_message}")
+
+        for line in file_read.readlines(): 
+            if (len(line) > len(info_header)):
+                if (line[0:len(info_header)] == info_header): 
+                    return(line[len(info_header) : len(line) : 1])
+        file_read.close()
+        return None
+    
+    def __init__(self, type): 
+        self.name = ""
+        self.path = ""
+        self.type = type
         
-    for line in file_read.readlines(): 
-        if (len(line) > len(info_header)):
-            if (line[0:len(info_header)] == info_header): 
-                return(line[len(info_header) : len(line) : 1])
-    file_read.close()
-    return None
+        if (self.type == "Original"): 
+            
+            self.root = self.retrieve_info ("setup.txt", "Path: b'", "")
+            self.root = self.root[0:len(self.root)-1:1]
+            self.name = "Oxford_17_Original"
+            self.path = self.root + f"/datasets/{self.name}"
+            
+        elif (self.type == "Copies"):
+            
+            self.root = self.retrieve_info("setup.txt", "Path: b'", "")
+            self.root = self.root[0:len(self.root)-1:1]
 
-# Dataset Name
-name = "Oxford_17_Original"
+            new_name = ""
 
-# Root path
-root = retrieve_info("setup.txt", "Path: b'", "Make sure that you have finished the setup module in the beginning of the notebook. ")
-root = root[0:len(root)-1:1]
+            os.system("clear")
+            while (new_name == ""): 
+                
+                n = input("Input a name for the copy of Oxford 17 flowers dataset. ")
+                for i in range(len(n)): 
+                    if ((n[i] >= '0' and n[i] <= '9') or (n[i] >= 'a' and n[i] <= 'z') or (n[i] >= 'A' and n[i] <= 'Z') or (n[i] == '_')):
+                        new_name += n[i]
+                if (new_name == ""): continue
+                new_name = "Oxford_17_" + new_name
+                all_dataset = [f.name for f in os.scandir(f"{self.root}/datasets") if f.is_dir()]
+                # print(all_dataset)
+                if (new_name in all_dataset): 
+                    input (f"The dataset {new_name} already exists. ")
+                    new_name = ""
+                confirm = ""
+                confirm = input(f"Name: {new_name}. Press Enter to confirm. Enter anything to discard. ")
+                if (confirm != ""): new_name = ""
+                    
+            new_path = self.root + f"/datasets/{new_name}"
+            print (f"The new dataset {new_name} is created. ")
+            print (f"Dataset path: {new_path}")
+            self.name = new_name
+            self.path = new_path
+        
+        else: raise Exception (f"[Error] Invalid type: {self.type}")
 
-# Dataset path
-path = root + f"/datasets/{name}"
+        if (os.path.exists(self.path)): shutil.rmtree(self.path, ignore_errors=True)
+        print ("Downloading Oxford 17 flowers source data. ")
+        os.system("wget https://www.robots.ox.ac.uk/~vgg/data/flowers/17/17flowers.tgz")
+        os.system("tar zxvf 17flowers.tgz")
+        os.system(f"mv jpg datasets/{self.name}")
+        os.system("rm -rf 17flowers.tgz")
+        print ("Successfully downloaded Oxford 17 flowers source data. ")
 
-if (path == None): raise Exception(f"[Error] The root directory path does not exist. Make sure that you have finished the setup module in the beginning of the notebook. ")
+        print(f"Dataset path: {self.path}")
+        if (os.path.exists(self.path + "/train")): shutil.rmtree(self.path + "/train", ignore_errors=True)
+        if (os.path.exists(self.path + "/val")): shutil.rmtree(self.path + "/val", ignore_errors=True)
+        if (os.path.exists(self.path + "/test")): shutil.rmtree(self.path + "/test", ignore_errors=True)
 
-if (os.path.exists(path)): shutil.rmtree(path, ignore_errors=True)
+        self.oxford_17_configure()
 
-print ("Downloading Oxford 17 flowers dataset. ")
-os.system("wget https://www.robots.ox.ac.uk/~vgg/data/flowers/17/17flowers.tgz")
-os.system("tar zxvf 17flowers.tgz")
-os.system(f"mv jpg datasets/{name}")
-os.system("rm -rf 17flowers.tgz jpg")
-print ("Successfully downloaded Oxford 17 flowers dataset. ")
-print(f"Dataset path: {path}")
+        print ("Generating Oxford 17 flowers dataset. ")
 
-if (os.path.exists(path + "/train")): shutil.rmtree(path_1 + "/train", ignore_errors=True)
-if (os.path.exists(path + "/val")): shutil.rmtree(path_1 + "/val", ignore_errors=True)
-if (os.path.exists(path + "/test")): shutil.rmtree(path_1 + "/test", ignore_errors=True)
+        images = sorted([i for i in os.listdir(self.path) if "jpg" in i])
+        print (f"There is a total of {len(images)} images in the Oxford 17 flowers source data.\n")
+        images_perms = []
+        for i in range (17): 
+            images_perm = np.random.permutation(images[i*80: (i+1)*80])
+            images_perms.append(images_perm)
+        file_read = open("setup_dataset.txt", "r")
+        num_classes = 0
+        train, val, test = 0, 0, 0
+        dataset_ratio = 1
+        for line in file_read.readlines():
+            if (len(line) > 16 and line[0:16] == "Num of classes: "): 
+                num_classes = self.retrieve_info("setup_dataset.txt", "Num of classes: ", "")
+                num_classes = int(num_classes)
+                continue
+            if (len(line) > 16 and line[0:16] == "Train_Val_Test: "): 
+                train, val, test = self.retrieve_info("setup_dataset.txt", "Train_Val_Test: ", "").split(" ")
+                train = int(train); val = int(val); test = int(test)
+                continue
+            if (len(line) > 15 and line[0:15] == "Dataset ratio: "): 
+                dataset_ratio = self.retrieve_info("setup_dataset.txt", "Dataset ratio: ", "")
+                dataset_ratio = float(dataset_ratio)
+                continue
+        train_num = round(80 * train * dataset_ratio / (train + val + test))
+        val_num = round(80 * val * dataset_ratio / (train + val + test))
+        test_num = int(80 * dataset_ratio) - train_num - val_num
 
-file_write = open(f"dataset_config.txt", "w")
-file_write.write (f"Dataset: {name}")
-file_write.close()
+        os.makedirs(self.path + "/train", exist_ok = True)
+        os.makedirs(self.path + "/val", exist_ok = True)
+        os.makedirs(self.path + "/test", exist_ok = True)
+        for i in range(17):
+            class_name = "class_" + str(i + 1)
+            os.makedirs(self.path + f"/train/{class_name}", exist_ok=True)
+            os.makedirs(self.path + f"/val/{class_name}", exist_ok=True)
+            os.makedirs(self.path + f"/test/{class_name}", exist_ok=True)
+        train_images, val_images, test_images = [], [], []
+        for i in range (17):
+            train_images.extend(images_perms[i][0:train_num])
+            for j in range(int(train_num * dataset_ratio)):
+                os.system(f"cp \"{self.path}/{images_perms[i][j]}\" \"{self.path}/train/class_{i+1}\"")
+            val_images.extend(images_perms[i][train_num : train_num + val_num]) 
+            for j in range(int(val_num * dataset_ratio)):
+                os.system(f"cp \"{self.path}/{images_perms[i][train_num + j]}\" \"{self.path}/val/class_{i+1}\"")
+            test_images.extend(images_perms[i][train_num + val_num : 80])
+            for j in range(int(test_num * dataset_ratio)):
+                os.system(f"cp \"{self.path}/{images_perms[i][train_num + val_num + j]}\" \"{self.path}/test/class_{i+1}\"")
+        for i in range(len(images)): os.system(f"rm -rf \"{self.path}/{images[i]}\"")
+        os.system(f"rm -rf \"{self.path}/files.txt\"")
+        os.system(f"rm -rf \"{self.root}/datasets/jpg\"")
 
-print ("Setting up dataset configurations. ")
-import setup_dataset
-print ("Dataset configurations are all set. ")
 
-
-if (0):
-    
-    images = sorted([i for i in os.listdir(path) if "jpg" in i])
-    print (f"There is a total of {len(images)} images in the Dataset.\n")
-    images_perms = []
-    for i in range (17): 
-        images_perm = np.random.permutation(images[i*80: (i+1)*80])
-        images_perms.append(images_perm)
-    training_images_num = round(80 * training_image_ratio * dataset_ratio / (training_image_ratio + validation_image_ratio + testing_image_ratio))
-    validation_images_num = round(80 * validation_image_ratio * dataset_ratio / (training_image_ratio + validation_image_ratio + testing_image_ratio))
-    testing_images_num = int(80 * dataset_ratio) - training_images_num - validation_images_num
-
-    
-    try:
-        os.makedirs(path + "/training", exist_ok = True)
-        print ("The \"training\" folder: Successfully created. ")
-        try:
-            for i in range(17):
-                class_name = "class_" + str(i + 1)
-                os.makedirs(path + f"/training/{class_name}", exist_ok=True)
-            print ("The sub-folders under \"training\": Successfully created. ")
-        except OSError: print ("Error in creating sub-folders under \"training\". ")      
-    except OSError: print ("Error in creating the \"training\" folder. ")
-    try:
-        os.makedirs(path_1 + "/validation", exist_ok = True)
-        print ("The \"validation\" folder: Successfully created. ")  
-        try:
-            for i in range(17):
-                class_name = "class_" + str(i + 1)
-                os.makedirs(path_1 + f"/validation/{class_name}", exist_ok=True)
-            print ("The sub-folders under \"validation\": Successfully created. ")
-        except OSError: print ("Error in creating sub-folders under \"validation\". ")
-    except OSError: print ("Error in creating the \"validation\" folder. ")
-    try:
-        os.makedirs(path_1 + "/testing", exist_ok = True)
-        print ("The \"testing\" folder: Successfully created. ")
-        try:
-            for i in range(17):
-                class_name = "class_" + str(i + 1)
-                os.makedirs(path_1 + f"/testing/{class_name}", exist_ok=True)
-            print ("The sub-folders under \"testing\": Successfully created. ")
-        except OSError: print ("Error in creating sub-folders under \"testing\". ")
-    except OSError: print ("Error in creating the \"testing\" folder. ")
-    training_images = []
-    validation_images = []
-    testing_images = []
-    for i in range (17):
-        training_images.extend(images_perms[i][0:training_images_num])
-        for j in range(int(training_images_num * dataset_ratio)):
-            os.system(f"cp \"{path_1}/{images_perms[i][j]}\" \"{path_1}/training/class_{i+1}\"")
-        validation_images.extend(images_perms[i][training_images_num : training_images_num + validation_images_num]) 
-        for j in range(int(validation_images_num * dataset_ratio)):
-            os.system(f"cp \"{path_1}/{images_perms[i][training_images_num + j]}\" \"{path_1}/validation/class_{i+1}\"")
-        testing_images.extend(images_perms[i][training_images_num + validation_images_num : 80])
-        for j in range(int(testing_images_num * dataset_ratio)):
-            os.system(f"cp \"{path_1}/{images_perms[i][training_images_num + validation_images_num + j]}\" \"{path_1}/testing/class_{i+1}\"")
-    for i in range(len(images)): os.system(f"rm -rf \"{path_1}/{images[i]}\"")
-    os.system(f"rm -rf \"{path_1}/files.txt\"")
-    os.system(f"rm -rf \"{path_1}/files.txt~\"")
-
+    def oxford_17_configure(self): 
+        print ("Setting up dataset configurations. ")
+        import setup_dataset as S
+        S.setup_dataset(self.name)
+        print ("Dataset configurations are all set. ")
 
 
--- setup_dataset.py
+++ setup_dataset.py
@@ -3,79 +3,76 @@
 
 import os
 
-def retrieve_info(file, info_header, error_message): 
-    try: 
-        file_read = open(file, "r")
-    except FileNotFoundError: 
-        raise Exception(f"[Error] {file} does not exist. {error_message}")
-        
-    for line in file_read.readlines(): 
-        if (len(line) > len(info_header)):
-            if (line[0:len(info_header)] == info_header): 
-                return(line[len(info_header) : len(line) : 1])
-    file_read.close()
-    return None
+class setup_dataset: 
 
-# Root path
+    def retrieve_info(self, file, info_header, error_message): 
+        try: 
+            file_read = open(file, "r")
+        except FileNotFoundError: 
+            raise Exception(f"[Error] {file} does not exist. {error_message}")
 
-dataset = retrieve_info(f"dataset_config.txt", "Dataset: ", "")
-if (dataset == None): raise Exception("[Error] The dataset is not defined. ")
+        for line in file_read.readlines(): 
+            if (len(line) > len(info_header)):
+                if (line[0:len(info_header)] == info_header): 
+                    return(line[len(info_header) : len(line) : 1])
+        file_read.close()
+        return None
 
-# print("Dataset:", dataset)
+    def __init__(self, dataset): 
 
-
-if (dataset == "Oxford_17_Original"): 
-    num_classes = 17
-else: 
-    print("")
-    print("Input an positive integer to indicate the total number of classes in the dataset. ")
-    num_classes = None
-    while (num_classes == None):
-        os.system("clear")
-        num_classes = input("Number of classes in the dataset: ")
-        if (not num_classes.isdigit()): 
+        flag = False
+        if (len(dataset) > 10): 
+            if (dataset[0:10] == "Oxford_17_"):
+                num_classes = 17
+                flag = True
+        if (not flag): 
+            print("")
+            print("Input an positive integer to indicate the total number of classes in the dataset. ")
             num_classes = None
-        else: num_classes = int(num_classes)
+            while (num_classes == None):
+                os.system("clear")
+                num_classes = input("Number of classes in the dataset: ")
+                if (not num_classes.isdigit()): 
+                    num_classes = None
+                else: num_classes = int(num_classes)
 
-print("")
-print ("Give 3 positive integers, seperated by a space, to indicate the ratio of training, validation and testing images")
-print ("For example, input \"8 1 1\" will set the ratio 8:1:1. ")
-train, val, test = None, None, None
-while ((train and val and test) == None):
-    try:
-        train, val, test = input("{training : validation : testing} = ").split(' ')
-    except: train, val, test = None, None, None
-    if (train != None): 
-        if (not train.isdigit()): 
-            train = None
-        else: train = int(train)
-    if (val != None):
-        if (not val.isdigit()): 
-            val = None
-        else: val = int(val)
-    if (test != None):
-        if (not test.isdigit()): 
-            test = None
-        else: test = int(test)
-        
-print("")
-print ("Give a ratio in range (0, 1] to indicate the proportion of the dataset to be used. ")
-print ("For example, input \"0.6\" will use 60% of the dataset. ")
-dataset_ratio = None
-while (dataset_ratio == None): 
-    try:
-        dataset_ratio = float(input("Dataset ratio: "))
-        if (not (0 < dataset_ratio and dataset_ratio <= 1)):
-            dataset_ratio = None
-    except ValueError: dataset_ratio = None
+        print("")
+        print ("Give 3 positive integers, seperated by a space, to indicate the ratio of training, validation and testing images")
+        print ("For example, input \"8 1 1\" will set the ratio 8:1:1. ")
+        train, val, test = None, None, None
+        while ((train and val and test) == None):
+            try:
+                train, val, test = input("{training : validation : testing} = ").split(' ')
+            except: train, val, test = None, None, None
+            if (train != None): 
+                if (not train.isdigit()): 
+                    train = None
+                else: train = int(train)
+            if (val != None):
+                if (not val.isdigit()): 
+                    val = None
+                else: val = int(val)
+            if (test != None):
+                if (not test.isdigit()): 
+                    test = None
+                else: test = int(test)
 
+        print("")
+        print ("Give a ratio in range (0, 1] to indicate the proportion of the dataset to be used. ")
+        print ("For example, input \"0.6\" will use 60% of the dataset. ")
+        dataset_ratio = None
+        while (dataset_ratio == None): 
+            try:
+                dataset_ratio = float(input("Dataset ratio: "))
+                if (not (0 < dataset_ratio and dataset_ratio <= 1)):
+                    dataset_ratio = None
+            except ValueError: dataset_ratio = None
 
-        
-file_write = open(f"setup_dataset.txt", "w")
-file_write.write(f"Num of classes: {num_classes}")
-file_write.write(f"Train_Val_Test: {train} {val} {test}")
-file_write.write(f"Dataset ratio: {dataset_ratio}")
-file_write.close()
+        file_write = open(f"setup_dataset.txt", "w")
+        file_write.write(f"Num of classes: {num_classes}\n")
+        file_write.write(f"Train_Val_Test: {train} {val} {test}\n")
+        file_write.write(f"Dataset ratio: {dataset_ratio}\n")
+        file_write.close()
 
-print("")
+        print("")
 
