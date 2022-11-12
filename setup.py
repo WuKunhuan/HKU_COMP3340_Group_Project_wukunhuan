95a9279a57adfdcabcf7f5c4bbdd829913c4ae42
WuKunhuan <u3577163@connect.hku.hk>
2022-11-12 16:01:04 +0800

    [Recover] Updated setup.py

--- setup.py
+++ setup.py
@@ -1,44 +1,42 @@
 
 import os, sys, subprocess
-os.system("clear")
 
-# Check the system
-if (os.name == 'nt'):
-    raise Exception (f"[Error] Please use Linux or Mac, but not Windows to work with this notebook")
+class setup:
+    
+    def __init__(self): 
+        os.system("clear")
+        if (os.name == 'nt'):
+            raise Exception (f"[Error] Please use Linux or Mac, but not Windows to work with this notebook")
 
-# Setting the environment path
-path = subprocess.check_output("pwd")
-path = path[0:len(path)-1]
-print(f"Path: {path}")
-print("")
+        self.path = str(subprocess.check_output("pwd"))
+        self.path = self.path[2:len(self.path)-3:1]
+        print(f"Path: {self.path}")
 
-file_output = open("setup.txt", "w")
-file_output.write(f"Path: {path}")
-file_output.close()
+        # Import Pytorch
+        try:
+            import torch
+            import torch.nn as nn
+        except ImportError:
+            os.system("clear")
+            raise Exception(f"[Error] import pytorch failed. Run \"conda install pytorch\" in a terminal under {path}, then run the setup again. ")
+        print("[Package] import pytorch succeed. ")
 
-# Import Pytorch
-try:
-    import torch
-    import torch.nn as nn
-except ImportError:
-    os.system("clear")
-    raise Exception(f"[Error] import pytorch failed. Run \"conda install pytorch\" in a terminal under {path}, then run the setup again. ")
-print("[Package] import pytorch succeed. ")
+        # Import numpy
+        try:
+            import numpy as np
+        except ImportError:
+            os.system("clear")
+            raise Exception(f"[Error] import numpy failed. Run \"conda install numpy\" in a terminal under {path}, then run the setup again. ")
+        print("[Package] import numpy succeed. ")
 
-# Import numpy
-try:
-    import numpy as np
-except ImportError:
-    os.system("clear")
-    raise Exception(f"[Error] import numpy failed. Run \"conda install numpy\" in a terminal under {path}, then run the setup again. ")
-print("[Package] import numpy succeed. ")
-
-# Import torchvision
-try:
-    import torchvision
-    from torchvision import transforms
-except FileNotFoundError:
-    os.system("clear")
-    raise Exception(f"[Error] import torchvision failed. Run \"conda install torchvision\" in a terminal under {path}, then run the setup again. ")
-print("[Package] import torchvision succeed. ")
+        # Import torchvision
+        try:
+            import torchvision
+            from torchvision import transforms
+        except FileNotFoundError:
+            os.system("clear")
+            raise Exception(f"[Error] import torchvision failed. Run \"conda install torchvision\" in a terminal under {path}, then run the setup again. ")
+        print("[Package] import torchvision succeed. ")
+        
+        
 
