fa88eb200ec39fc467832c2e16db35c89a9283fc
WuKunhuan <u3577163@connect.hku.hk>
2022-11-21 19:43:41 +0800

    [Fix]  Fixed Inception_V1 model

--- models/Inception_V1.py
+++ models/Inception_V1.py
@@ -1,118 +1,179 @@
 
+'''
+Reference: https://github.com/antspy/inception_v1.pytorch/blob/master/inception_v1.py
+'''
+
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import os
 
-class Conv_block(nn.Module):
-    def __init__(self, in_channels, out_channels, **kwargs):
-        super(Conv_block, self).__init__()
-        self.relu = nn.ReLU()
-        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
-        self.batchnorm = nn.BatchNorm2d(out_channels)
-    def forward(self, x):
-        return self.relu(self.batchnorm(self.conv(x)))
+def layer_init(m):
     
-class Inception_block(nn.Module):
-    
-    def __init__(self, in_channels, out_1x1, red_3x3, out_3x3, red_5x5, out_5x5, out_1x1pool):
-        super(Inception_block, self).__init__()
-        self.branch1 = Conv_block(in_channels, out_1x1, kernel_size=(1, 1))
-        self.branch2 = nn.Sequential(
-        Conv_block(in_channels, red_3x3, kernel_size=(1, 1)),
-        Conv_block(red_3x3, out_3x3, kernel_size=(3, 3), padding=(1, 1)))
-        self.branch3 = nn.Sequential(
-        Conv_block(in_channels, red_5x5, kernel_size=(1, 1)),
-        Conv_block(red_5x5, out_5x5, kernel_size=(5, 5), padding=(2, 2)))
-        self.branch4 = nn.Sequential(
-        nn.MaxPool2d(kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-        Conv_block(in_channels, out_1x1pool, kernel_size=(1, 1)))
+    classname = m.__class__.__name__
+    classname = classname.lower()
+    if classname.find('conv') != -1 or classname.find('linear') != -1:
+        gain = nn.init.calculate_gain(classname)
+        nn.init.xavier_uniform(m.weight, gain=gain)
+        if m.bias is not None:
+            nn.init.constant(m.bias, 0)
+    elif classname.find('batchnorm') != -1:
+        nn.init.constant(m.weight, 1)
+        if m.bias is not None:
+            nn.init.constant(m.bias, 0)
+    elif classname.find('embedding') != -1:
+        # The default initializer in the TensorFlow embedding layer is a truncated normal with mean 0 and
+        # standard deviation 1/sqrt(sparse_id_column.length). Here we use a normal truncated with 3 std dev
+        num_columns = m.weight.size(1)
+        sigma = 1/(num_columns**0.5)
+        m.weight.data.normal_(0, sigma).clamp_(-3*sigma, 3*sigma)
+
+class LRN(nn.Module):
+
+    '''
+    Implementing Local Response Normalization layer. Implemention adapted
+    from https://github.com/jiecaoyu/pytorch_imagenet/blob/master/networks/model_list/alexnet.py
+    '''
+
+    def __init__(self, local_size=1, alpha=1.0, beta=0.75, k=1, ACROSS_CHANNELS=False):
+        super(LRN, self).__init__()
+        self.ACROSS_CHANNELS = ACROSS_CHANNELS
+        if ACROSS_CHANNELS:
+            self.average=nn.AvgPool3d(kernel_size=(local_size, 1, 1),
+                    stride=1,
+                    padding=(int((local_size-1.0)/2), 0, 0))
+        else:
+            self.average=nn.AvgPool2d(kernel_size=local_size,
+                    stride=1,
+                    padding=int((local_size-1.0)/2))
+        self.alpha = alpha
+        self.beta = beta
+        self.k = k
 
     def forward(self, x):
-        return torch.cat([self.branch1(x), self.branch2(x), self.branch3(x), self.branch4(x)], 1)
-    
-class Inception_auxiliary(nn.Module):
-    
-    def __init__(self, in_channels, out_channels):
-        super(Inception_auxiliary, self).__init__()
-        self.pool = nn.AvgPool2d(kernel_size = 5, stride = 3)
-        self.conv = Conv_block(in_channels, 128, kernel_size = 1)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.7)
-        self.fc1 = nn.Linear(2048, 1024)
-        self.fc2 = nn.Linear(1024, out_channels)
-        self.softmax = nn.Softmax(dim=1)
-        
-    def forward(self, x):
-        x = self.pool(x)
-        x = self.conv(x)
-        x = x.reshape(x.shape[0], -1)
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.softmax(x)
+        if self.ACROSS_CHANNELS:
+            div = x.pow(2).unsqueeze(1)
+            div = self.average(div).squeeze(1)
+            div = div.mul(self.alpha).add(self.k).pow(self.beta)
+        else:
+            div = x.pow(2)
+            div = self.average(div)
+            div = div.mul(self.alpha).add(self.k).pow(self.beta)
+        x = x.div(div)
         return x
-    
+
+class Inception_base(nn.Module):
+    def __init__(self, depth_dim, input_size, config):
+        super(Inception_base, self).__init__()
+
+        self.depth_dim = depth_dim
+
+        #mixed 'name'_1x1
+        self.conv1 = nn.Conv2d(input_size, out_channels=config[0][0], kernel_size=1, stride=1, padding=0)
+
+        #mixed 'name'_3x3_bottleneck
+        self.conv3_1 = nn.Conv2d(input_size, out_channels=config[1][0], kernel_size=1, stride=1, padding=0)
+        #mixed 'name'_3x3
+        self.conv3_3 = nn.Conv2d(config[1][0], config[1][1], kernel_size=3, stride=1, padding=1)
+
+        # mixed 'name'_5x5_bottleneck
+        self.conv5_1 = nn.Conv2d(input_size, out_channels=config[2][0], kernel_size=1, stride=1, padding=0)
+        # mixed 'name'_5x5
+        self.conv5_5 = nn.Conv2d(config[2][0], config[2][1], kernel_size=5, stride=1, padding=2)
+
+        self.max_pool_1 = nn.MaxPool2d(kernel_size=config[3][0], stride=1, padding=1)
+        #mixed 'name'_pool_reduce
+        self.conv_max_1 = nn.Conv2d(input_size, out_channels=config[3][1], kernel_size=1, stride=1, padding=0)
+
+        self.apply(layer_init)
+
+    def forward(self, input):
+
+        output1 = F.relu(self.conv1(input))
+
+        output2 = F.relu(self.conv3_1(input))
+        output2 = F.relu(self.conv3_3(output2))
+
+        output3 = F.relu(self.conv5_1(input))
+        output3 = F.relu(self.conv5_5(output3))
+
+        output4 = F.relu(self.conv_max_1(self.max_pool_1(input)))
+
+        return torch.cat([output1, output2, output3, output4], dim=self.depth_dim)
+
+# weights available at https://github.com/antspy/inception_v1.pytorch
+
 class Inception_V1(nn.Module):
     
-    def __init__(self, num_classes, aux_logits = True, init_weights=False): 
+    def __init__(self, num_classes=17):
         super(Inception_V1, self).__init__()
-        self.aux_logits = aux_logits
-        self.conv_1 = Conv_block(in_channels=3, out_channels=64, kernel_size=(7, 7), stride=(2, 2), padding=(1, 1))
-        self.max_pool_1 = nn.MaxPool2d(kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-        self.conv_2 = Conv_block(in_channels=64, out_channels=64, kernel_size=(1, 1), stride=(1, 1), padding=(0, 0)) 
-        self.conv_3 = Conv_block(in_channels=64, out_channels=192, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        self.max_pool_2 = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
-        self.inception_3a = Inception_block(192, 64, 96, 128, 16, 32, 32)
-        self.inception_3b = Inception_block(256, 128, 128, 192, 32, 96, 64)
-        self.max_pool_3 = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
-        self.inception_4a = Inception_block(480, 192, 96, 208, 16, 48, 64)
-        self.inception_4b = Inception_block(512, 160, 112, 224, 24, 64, 64)
-        self.inception_4c = Inception_block(512, 128, 128, 256, 24, 64, 64)
-        self.inception_4d = Inception_block(512, 112, 144, 288, 32, 64, 64)
-        self.inception_4e = Inception_block(528, 256, 160, 320, 32, 128, 128)
-        self.max_pool_4 = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
-        self.inception_5a = Inception_block(832, 256, 160, 320, 32, 128, 128)
-        self.inception_5b = Inception_block(832, 384, 192, 384, 48, 128, 128)
-        self.avg_pool_1 = nn.AvgPool2d(kernel_size=(7, 7), stride=(1, 1), padding=(0, 0))
-        self.dropout = nn.Dropout(p=0.4)
-        self.fully_connected_1 = nn.Linear(1024, num_classes)
-        self.softmax = nn.Softmax(dim = 1)
-        if self.aux_logits:
-            self.auxiliary_1 = Inception_auxiliary(512, num_classes)
-            self.auxiliary_2 = Inception_auxiliary(528, num_classes)
+
+        #conv2d0
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.max_pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.lrn1 = LRN(local_size=11, alpha=0.00109999999404, beta=0.5, k=2)
+
+        #conv2d1
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=1, stride=1, padding=0)
+
+        #conv2d2
+        self.conv3  = nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1)
+        self.lrn3 = LRN(local_size=11, alpha=0.00109999999404, beta=0.5, k=2)
+        self.max_pool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.inception_3a = Inception_base(1, 192, [[64], [96,128], [16, 32], [3, 32]]) #3a
+        self.inception_3b = Inception_base(1, 256, [[128], [128,192], [32, 96], [3, 64]]) #3b
+        self.max_pool_inc3= nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
+
+        self.inception_4a = Inception_base(1, 480, [[192], [ 96,204], [16, 48], [3, 64]]) #4a
+        self.inception_4b = Inception_base(1, 508, [[160], [112,224], [24, 64], [3, 64]]) #4b
+        self.inception_4c = Inception_base(1, 512, [[128], [128,256], [24, 64], [3, 64]]) #4c
+        self.inception_4d = Inception_base(1, 512, [[112], [144,288], [32, 64], [3, 64]]) #4d
+        self.inception_4e = Inception_base(1, 528, [[256], [160,320], [32,128], [3,128]]) #4e
+        self.max_pool_inc4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.inception_5a = Inception_base(1, 832, [[256], [160,320], [48,128], [3,128]]) #5a
+        self.inception_5b = Inception_base(1, 832, [[384], [192,384], [48,128], [3,128]]) #5b
+        self.avg_pool5 = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
+
+        self.dropout_layer = nn.Dropout(0.4)
+        self.fc = nn.Linear(1024, num_classes)
+
+        self.apply(layer_init)
+
+    def forward(self, input):
+
+        output = self.max_pool1(F.relu(self.conv1(input)))
+        output = self.lrn1(output)
+
+        output = F.relu(self.conv2(output))
+        output = F.relu(self.conv3(output))
+        output = self.max_pool3(self.lrn3(output))
+
+        output = self.inception_3a(output)
+        output = self.inception_3b(output)
+        output = self.max_pool_inc3(output)
+
+        output = self.inception_4a(output)
+
+        output = self.inception_4b(output)
+        output = self.inception_4c(output)
+        output = self.inception_4d(output)
+
         
-    def forward(self, x):
-        
-        if (x.size()[1:4] != torch.Size([3, 224, 224])): 
-            raise Exception(f"Error: Inception_V1 input dimension expected torch.Size([3, 224, 224]), but {x.size()[-3:0:1]} received instead. ")
+        output = self.inception_4e(output)
+        output = self.max_pool_inc4(output)
+
+        output = self.inception_5a(output)
+        output = self.inception_5b(output)
+        output = self.avg_pool5(output)
+
+        output = output.view(-1, 1024)
+
+        if self.fc is not None:
             
-        aux1 = None; aux2 = None
-        x = self.conv_1(x)
-        x = self.max_pool_1(x)
-        x = self.conv_2(x)
-        x = self.conv_3(x)
-        x = self.max_pool_2(x)
-        x = self.inception_3a(x)
-        x = self.inception_3b(x)
-        x = self.max_pool_3(x)
-        x = self.inception_4a(x)
-        if self.training and self.aux_logits: 
-            aux1 = self.auxiliary_1(x)
-        x = self.inception_4b(x)
-        x = self.inception_4c(x)
-        x = self.inception_4d(x)
-        if self.training and self.aux_logits: 
-            aux2 = self.auxiliary_2(x)
-        x = self.inception_4e(x)
-        x = self.max_pool_4(x)
-        x = self.inception_5a(x)
-        x = self.inception_5b(x)
-        x = self.avg_pool_1(x)
-        x = torch.flatten(x, 1)
-        x = self.dropout(x)
-        x = self.fully_connected_1(x)
-        x = self.softmax(x)
-        # print ("Inception_V1 network forward propagation done. ")
-                
-        if self.aux_logits and self.training: 
-            return aux1, aux2, x
-        else: return x
+            output = self.dropout_layer(output)
+            output = self.fc(output)
+
+        return output
