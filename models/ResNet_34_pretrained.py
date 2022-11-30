17a82b3901aa1785f3b1dd55a0c897632502ade1
huarq28 <67198639+huarq28@users.noreply.github.com>
2022-11-30 13:42:17 +0800

    Add files via upload

---
+++ models/ResNet_34_pretrained.py
@@ -0,0 +1,18 @@
+
+# Ref: https://pytorch.org/vision/0.8/models.html
+# Ref: https://pytorch.org/vision/stable/models.html
+# Ref: https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py
+
+
+import torch
+import torch.nn as nn
+from torchvision.io import read_image
+from torchvision.models import resnet34
+        
+def ResNet_34_pretrained():
+    
+    model = resnet34(pretrained=True)
+    model.fc = nn.Linear(512, 17)
+    return model
+ 
+        
